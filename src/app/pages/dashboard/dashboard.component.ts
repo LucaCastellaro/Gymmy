0d39fb2da87dec437289fd8d476da1ff3be24187
Luca <luca.castellaro.lc@gmail.com>
2022-08-10 21:32:31 +0200

    Add dashboard component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  public exercises: any[] = [
+    {
+      id: '1',
+      name: 'Esercizio 1',
+    },
+    {
+      id: '2',
+      name: 'Esercizio 2',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { ComponentsModule } from '../components/components.module';
 import { ZorroModule } from '../zorro/zorro.module';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+import { CheckCircleOutline, PlusOutline } from '@ant-design/icons-angular/icons';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+
+const icons = [ CheckCircleOutline, PlusOutline ];
 
 const components = [
     AuthComponent,
@@ -14,7 +23,11 @@ const components = [
     imports: [
         CommonModule,
         ZorroModule,
-        ComponentsModule
+        ComponentsModule,
+        NzCardModule,
+        NzButtonModule,
+        NzListModule,
+        NzIconModule.forRoot(icons)
     ],
     exports: components,
     declarations: components,
