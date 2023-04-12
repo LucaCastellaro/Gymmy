6d3c46efd3e9b5b9a339fe9cfb7c07ccd31b05ea
Luca <luca.castellaro.lc@gmail.com>
2023-04-12 22:38:59 +0200

    Add react query

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -2,24 +2,24 @@ import { decrement, increment } from '../redux/feature/counter/counterSlice'
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 
 export const Counter = (): JSX.Element => {
-  const count = useAppSelector(state => state.counter.value);
+    const count = useAppSelector(state => state.counter.value);
   const dispatch = useAppDispatch();
 
-  return (
-    <div>
-        <button
-            aria-label="+1"
-            onClick={() => dispatch(increment())}
-        >
-            +1
-        </button>
-        <span>{count}</span>
-        <button
-            aria-label="-1"
-            onClick={() => dispatch(decrement())}
-        >
-            -1
-        </button>
+    return (
+        <div>
+            <button
+                aria-label="+1"
+                onClick={() => dispatch(increment())}
+            >
+                +1
+            </button>
+            <span>{count}</span>
+            <button
+                aria-label="-1"
+                onClick={() => dispatch(decrement())}
+            >
+                -1
+            </button>
         </div>
-  )
+    )
 }
